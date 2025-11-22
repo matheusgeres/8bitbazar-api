ae28d740fe9c11d7de9f0ae765b2a859a476c45a
Matheus Geres <matheusgeres@gmail.com>
2025-11-22 19:28:53 -0300

    chore: upgrade Gradle to version 9.2.1 and set Java source compatibility to version 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ group = "br.com.eightbitbazar"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_25
 }
 
 repositories {
