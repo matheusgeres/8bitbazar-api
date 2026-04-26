fbcfee9e81fce2e605bfb5d2843f5188f31082d0
Matheus Geres <matheusgeres@gmail.com>
2026-04-26 18:06:56 -0300

    fix: upgrade logstash-logback-encoder to 8.1 (Logback 1.5.x compat) and add test profile

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
 
     // Monitoring
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+    implementation("net.logstash.logback:logstash-logback-encoder:8.1")
     implementation("org.springframework.boot:spring-boot-health")
     implementation("io.micrometer:micrometer-registry-prometheus")
 
